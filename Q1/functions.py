be1c9e1fcf4acbf1e63866963880b697b488bd50
fazerneo <35126921@student.murdoch.edu.au>
2024-05-07 00:16:31 +0800

    changes

--- Q1/functions.py
+++ Q1/functions.py
@@ -1,8 +1,43 @@
 import csv
 import os
 
+def menu():
+    ''' This function prints out a menu for the main program and gets user input,
+    the user input is then returned for use in the main program '''
+    
+    print("Sales Records Management System\n")
+    print("Please select an option below to get started")
+    print("1. Load Customer and Sales Records")
+    print("2. Save Customer Records")
+    print("3. Save Sales Records")
+    print("4. Quit Program")
+    user_input = input("Type the number corresponding to your desired action: ")
+    
+    return user_input
+
+def other_options(user_input):
+    if user_input == 1:
+        return option1()
+    elif user_input == 2:
+        return option2()
+    elif user_input == 3:
+        return option3()
+    
 def option1():
-    with open("/Q1 tests/customers.csv") as file1, open("/Q1 tests/sales.csv") as file2:
+    file1_name = input("please provide the filename or filepath of customer records:")
+    file2_name = input("please provide the filename or filepath of Sales record:")
+    with open(file1_name, "x") as file1, open(file2_name, "x") as file2:
+        reader1 = file1.read()
+        reader2 = file2.read()
+        lines = reader1.readlines()
+        return print(lines)
+    
+def option2():
+    print("no")
+    
+def option3():
+    print("ok")
+    
         
         
 
--- Q1/question_1.py
+++ Q1/question_1.py
@@ -1,36 +1,14 @@
-from tkinter import *
-from tkinter import ttk
+from functions import menu, other_options
 
+Loaded_records = {}
 
-option1_records = {}
-
-while True
-root = Tk()
-root.title("Sales Records Management System")
-window = ttk.Frame(root, padding = 10)
-window.grid(row=8, column=3)
-
-subtitle = ttk.Label(window, text="Select an option from below to get started").grid(row=0)
-space1 = ttk.Label(window, text="").grid(column=0, row=1)
-label1 = ttk.Label(window, text="1. Load Customer and Sales Records").grid(column=0, row=2, sticky="w")
-button1 = ttk.Button(window, text="Select", command=option1).grid(column=1, row=2)
-space2 = ttk.Label(window, text="").grid(column=0, row=3)
-label2 = ttk.Label(window, text="2. Save Customer Records").grid(column=0, row=4, sticky="w")
-button2 = ttk.Button(window, text="Select", command=root.destroy).grid(column=1, row=4)
-space3 = ttk.Label(window, text="").grid(column=0, row=5)
-label3 = ttk.Label(window, text="3. Save Sales Records").grid(column=0, row=6, sticky="w")
-button3 = ttk.Button(window, text="Select", command=root.destroy).grid(column=1, row=6)
-space4 = ttk.Label(window, text="").grid(column=0, row=7)
-label4 = ttk.Label(window, text="4. Quit Program").grid(column=0, row=8, sticky="w")
-Quit = ttk.Button(window, text="Select", command=root.destroy).grid(column=1, row=8)
-
-option1 = ttk.frame(root, padding = 10)
-option1.grid(row=8, column=3)
-
-subtitle = ttk.Label(window, text="Select an option from below to get started").grid(row=0)
-space1 = ttk.Label(window, text="").grid(column=0, row=1)
-label1 = ttk.Label(window, text="1. Load Customer and Sales Records").grid(column=0, row=2, sticky="w")
-button1 = ttk.Button(window, text="Select", command=root.destroy).grid(column=1, row=2)
+while True: 
+    user_input = menu()
+    user_input = int(user_input)
+    if user_input == 4:
+        print("\nThank you for using the program")
+        break
+    else:
+        other_options(user_input)
     
-root.mainloop()
-
+    
