d8d9a0fea59d87aac44313a4a7b7c80a7eb5be4f
fazerneo <35126921@student.murdoch.edu.au>
2024-05-07 23:06:51 +0800

    test

--- Q1/question_1.py
+++ Q1/question_1.py
@@ -5,11 +5,12 @@ Loaded_records = {}
 while True: 
     user_input = menu()
     user_input = int(user_input)
-    if user_input == 4:
+    
+    if user_input == 1:
+        Loaded_records = option1(Loaded_records)
+        print(Loaded_records)
+        
+    elif user_input == 4:
         print("\nThank you for using the program")
         break
-    elif user_input == 1:
-        option1(Loaded_records)
         
-    
-    
--- Q1/test.py
+++ Q1/test.py
@@ -1,30 +1,11 @@
-import csv
+# Initialize an empty dictionary
+my_dict = {'key': 9}
 
-customers = {}
-sales = {}
-#file1_name = input("please provide the filename or filepath of customer records:")
-#file2_name = input("please provide the filename or filepath of Sales record:")
+# Check if the dictionary is empty
+if my_dict:
+    print("no, the dictionary is empty.")
+else:
+    print("yes, the dictionary is not empty.")
 
-with open("Q1 tests/customers.csv", newline="", encoding="utf-8-sig") as file1, open("Q1 tests/sales.csv", newline="", encoding="utf-8-sig") as file2:
-    reader1 = csv.DictReader(file1)
-    reader2 = csv.DictReader(file2) 
-    
-    for row in reader1:
-        customers[row["cust_id"]] = {
-            "cust_id": row["cust_id"],
-            "name": row["name"],
-            "postcode": row["postcode"],
-            "phone number": row["phone number"]
-        }
-    
-    for row in reader2:
-        sales[row["trans_id"]] = {
-            "date": row["date"],
-            "trans_id": row["trans_id"],
-            "customer_id": row["customer_id"],
-            "category": row["category"],
-            "value": row["value"]
-        }
-    
     
     
